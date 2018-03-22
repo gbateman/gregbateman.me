195c2948e8b5a4812d0ea1fdae38d2935af3fdfa
Greg Bateman <gerbot150@gmail.com>
2018-03-22 15:18:21 -0400

    Dockerized

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
   path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
